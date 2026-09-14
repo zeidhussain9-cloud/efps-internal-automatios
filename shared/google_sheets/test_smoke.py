ba864460ed385ff9e623fe395d8998bc1269d08e
zeidhussain9-cloud <zeidhussain9@gmail.com>
2026-09-15 00:55:26 +0530

    Add Google Sheets package smoke test

---
+++ shared/google_sheets/test_smoke.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from client import GoogleSheetsClient, GoogleSheetsCredentials
+
+
+def test_google_sheets_client_constructs_with_injected_client() -> None:
+    fake = object()
+    client = GoogleSheetsClient(GoogleSheetsCredentials({"type": "service_account"}), client=fake)
+    assert client.client is fake
